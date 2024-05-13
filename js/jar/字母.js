6d7a3d8e0bb7ad3cf3babf8011847aa0c4d7782e
学英语 <166593756+iiiwc@users.noreply.github.com>
2024-05-13 14:15:56 +0800

    Create 字母.js

---
+++ js/jar/字母.js
@@ -0,0 +1,8 @@
+{
+    //https://zimu.app/
+    "搜索":"{wd}",
+    "搜索url":"https://zimu.app/vodsearch.html?wd={wd}&submit=",
+    "影片类型":"🇨🇳微博@学英语i",
+    "分类":"字母$1#国产$2#剧情$3#动漫$4#传媒$5#伦理$36#",
+    "分类url":"https://zimu.app/vodshow/area/{area}/by/{by}/class/{class}/id/{cateId}/lang/{lang}/year/{year}/page/{catePg}.html"
+}
