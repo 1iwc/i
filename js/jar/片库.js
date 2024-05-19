7dca74c23145405250679adf32f400ea3f9ce201
学英语 <166593756+iiiwc@users.noreply.github.com>
2024-05-19 18:53:46 +0800

    Add files via upload

---
+++ js/jar/片库.js
@@ -0,0 +1,9 @@
+{
+    //https://ainio.cn/
+    "搜索":"{wd}",
+    "搜索url":"https://ainio.cn/search/-------------.html?wd={wd}",
+    "影片状态":"来自🇨🇳微博@学英语i",
+    "影片类型":"🇨🇳微博@学英语i",
+    "分类":"电影$1#电视剧$2#综艺$3#动漫$38#短剧$48#",
+    "分类url":"https://ainio.cn/vodtype/show/area/{area}/by/{by}/class/{class}/id/{cateId}/lang/{lang}/year/{year}/page/{catePg}.html"
+}
