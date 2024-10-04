93b37ca6af303fb8f0393bd2f8db39854bde6baa
学英语 <166593756+1iwc@users.noreply.github.com>
2024-10-04 12:00:30 +0800

    Delete js/jar directory

--- js/jar/得利.js
+++
@@ -1,9 +0,0 @@
-{
-    //https://lib.ifabu.vip/
-    "搜索":"{wd}",
-    "搜索url":"https://www.libvio.pw/search/-------------.html?wd={wd}&submit=",
-    "影片状态":"来自🇨🇳微博@学英语i",
-    "影片类型":"🇨🇳微博@学英语i",
-    "分类":"电影$1#电视剧$2#动漫$4#日韩$15#欧美$16#",
-    "分类url":"https://www.libvio.pw/type/show/area/{area}/by/{by}/class/{class}/id/{cateId}/lang/{lang}/year/{year}/page/{catePg}.html"
-}
