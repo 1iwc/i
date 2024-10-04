1cae7d177ce7b16e7c99f525ceb64a8a47e3b59b
学英语 <166593756+1iwc@users.noreply.github.com>
2024-10-04 12:07:27 +0800

    Add files via upload

---
+++ js/jar/胖虎.js
@@ -0,0 +1,9 @@
+{
+    //https://www.cdnkan.top/
+    "搜索":"{wd}",
+    "搜索url":"https://www.phyt.net/phsch.html?wd={wd}",
+    "影片状态":"来自🇨🇳微博@学英语i",
+    "影片类型":"🇨🇳微博@学英语i",
+    "分类":"电影$1#电视剧$2#综艺$3#动漫$4#短剧$7#",
+    "分类url":"https://www.phyt.net/vodshow/{cateId}/area/{area}/by/{by}/class/{class}/lang/{lang}/page/{catePg}/year/{year}.html"
+}
